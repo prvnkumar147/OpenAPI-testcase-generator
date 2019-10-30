2ff522df274bd5fa5c22dc6967980b71ca423e33
prvnkumar147 <prvnkumar147@gmail.com>
2019-10-30 20:19:04 +0530

    WIP chang runtestcase file to run with timeout set

--- RunTestCases.js
+++ RunTestCases.js
@@ -2,39 +2,56 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const path = require('path')
 const Mocha = require('mocha')
+const execFile = require("child_process").execFile;
 
 const testFolder = './test6';
 const writeDir = "./TestFilesReady"
 
-fs.readdir(testFolder, (err, files) => {
-    files.forEach(fn => {
-	    var rFile = `${testFolder}/${fn}`;
-	    var wFile = `${writeDir}/${fn}`;
-	    var fc = fs.readFileSync(rFile).toString();
+const mocha = new Mocha();
+//mocha.options.timeout = 15000;
+mocha.options.opts = "./TestFilesReady/mocha.opts";
 
-		fs.createReadStream('out.csv')
-			.pipe(csv())
-			.on('data', (row) => {
-    
-			if(row['TestParam'].includes("REQUEST_BODY")){
-				var paramValue = row["ParamValue"].replace("“", '"')
-					.replace("”", '"')
-					.replace("‘", "'")
-					.replace("’", "'")
-				fc = fc.replace(`"${row['TestParam']}"`, paramValue);
-			} else{
-				fc = fc.replace(row['TestParam'], row['ParamValue']);
+var testFilesReady = fs.readdirSync(testFolder);
+const totalFiles = testFilesReady.length;
+var fileIndex = 0;
+
+testFilesReady.forEach(fn => {
+    var rFile = `${testFolder}/${fn}`;
+    var wFile = `${writeDir}/${fn}`;
+    var fc = fs.readFileSync(rFile).toString();
+
+	fs.createReadStream('out.csv')
+		.pipe(csv())
+		.on('data', (row) => {
+	//console.log(row);
+	if(row['TestParam'].includes("REQUEST_BODY")){
+		var paramValue = row["ParamValue"].replace("“", '"')
+			.replace("”", '"')
+			.replace("‘", "'")
+			.replace("’", "'")
+		fc = fc.replace(`"${row['TestParam']}"`, paramValue);	
+	} else{
+		fc = fc.replace(row['TestParam'], row['ParamValue']);
+	}
+		
+	}).on("end", (err) => {
+		fs.writeFile(wFile, fc, (err) => {
+			fileIndex = fileIndex + 1;
+
+			if (!err) {
+				console.log(`File written successfully => ${wFile}.`);
 			}
-		}).on("end", (err) => {
-			fs.writeFile(wFile, fc, (err) => {
-				if (!err) {
-					console.log(`File written successfully => ${wFile}.`);
-					var mocha = new Mocha();
-					mocha.addFile(wFile)
-					mocha.run();
-				}
-			});
+			if (fileIndex == totalFiles) {
+				console.log("Added all the tests files to Mocha...");
+				execFile('/home/praveen/Projects/apitive-testcase-generator/node_modules/mocha/bin/mocha', ['--timeout 15000', '/home/praveen/Projects/apitive-testcase-generator/TestFilesReady/'], (error, stdout, stderr) => {
+					if (error) {
+						throw error;
+					}
+					console.log("STDOUT START:");
+					console.log(stdout);
+					console.log("STDOUT END");
+				});
+			}
 		});
 	});
-	console.log(files);
 });
